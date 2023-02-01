4364db4291330006dae962ab5d0ab65ef1cf1559
bella3212 <106274360+bella3212@users.noreply.github.com>
2023-02-01 15:13:28 +0200

    Update generic2.js

--- client/src/utils/generic2.js
+++ client/src/utils/generic2.js
@@ -3,32 +3,38 @@ import "../Styles/GlobalStyle.css"
 import "../fonts/Ubuntu-Regular.ttf"
 
 
-export function Add_form_for_search(lable_text) {
+export function Add_form_for_search(lable_text, filters_from_config) {
     let next_page_route = '/Home/TPage/'
-    const [pk_search, setSearch] = useState("");
-
-    //const [input, setInput] = useState("");
-    console.log(lable_text)
-    console.log("***")
+    const [pk_search, setSearch] = useState({});
+    let filters_list = filters_from_config.split(',')
     const handleSubmit = (event) => {
+        console.log("handleSubmit")
+        let filter_key = document.getElementById("filter_key_" + lable_text).value
+        let filter_value = document.getElementById("filter_value_" + lable_text).value
+        let dict = {}
+        dict[filter_key] = filter_value
         event.preventDefault();
-        setSearch(event.target[0].value);
+        setSearch(dict);
         console.log(pk_search)
-        if (pk_search > 0) {
-            window.location.href = next_page_route+lable_text +'/'+ pk_search
-        }
-    };
-    // const search = (event) => {
-    //     if (event.key === 'Enter') {
-    //         alert(event.value);
-    //     }
-    // }
-    return (
-        <form onSubmit={handleSubmit} class="sideBySideForm">
 
-            {/* <input type="text" placeholder="some text" class="search" onkeydown="search(this)" /> */}
-            <input type="text" />
-            <button class="btnStyle" type="submit"><span class="spStyle">Search {lable_text}</span></button>
+        window.location.href = next_page_route + filter_key + '/' + filter_value
+
+    };
+    return (
+
+        <form onSubmit={handleSubmit} class="sideBySideForm">
+            <div class="row">
+                <div class="column"> {lable_text}</div>
+                <input class="form-control" type="text" id={"filter_key_" + lable_text} list={lable_text} autocomplete="off" />
+                <datalist id={lable_text} >
+                    {filters_list.map((item) =>
+                        <option value={item}></option>
+                    )}
+                </datalist>
+                <input type="text" id={"filter_value_" + lable_text} />
+                <button class="btnStyle" type="submit"><span class="spStyle">Search</span></button>
+
+            </div>
         </form>
     )
 
